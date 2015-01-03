a9856228531537f691ebb68d232ee40b0d5c23e5
Greg Van Buren <g.vanburen@gmail.com>
2015-01-03 14:32:27 -0800

    Updated sound

--- js/app.js
+++ js/app.js
@@ -9,25 +9,28 @@ $(document).ready(function () {
 	})
 
 	.mousedown(function() {
-		console.log('mousedown');
-		// play hadouken sound
+		playHadouken();
 		$('.ryu-ready').hide();
 		$('.ryu-throwing').show();
 		$('.hadouken').show();
-		// animate it to the right of the screen
+		$('.hadouken').finish().show()
+			.animate (
+				{'left':'300px'},
+				500,
+				function() {
+					$(this).hide();
+					$(this).css('left','-212px');
+				}
+			);
 	})
 	.mouseup(function() {
-		console.log('mouseup');
 		$('.ryu-ready').show();
 		$('.ryu-throwing').hide();
 	});
-
-
-
-
-
-
-
-
-
 });
+
+function playHadouken () {
+	$('#hadouken-sound')[0].volume = 0.5;
+	$('#hadouken-sound')[0].load();
+	$('#hadouken-sound')[0].play();
+}
